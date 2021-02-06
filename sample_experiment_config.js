744d8c4a6011f18babfeecdf29fdd70a42c490cf
Richie Casto <rcasto92@gmail.com>
2021-02-05 23:41:46 -0500

    add sample experiment config

---
+++ sample_experiment_config.js
@@ -0,0 +1,10 @@
+export const sampleExperimentConfig = {
+    "experiment1": {
+        "active": true,
+        "traffic_allocation": 100,
+        "variations": {
+            "control": 50,
+            "treatment": 50
+        }
+    }
+};
