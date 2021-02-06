c230caafcf4e2e6fb410f77c331f684ea77f1d63
Richie Casto <rcasto92@gmail.com>
2021-02-05 23:42:04 -0500

    package.json udpates, initial index

---
+++ index.js
@@ -0,0 +1,8 @@
+import murmurhash from 'murmurhash';
+import { sampleExperimentConfig } from './sample_experiment_config.js';
+
+const num_buckets = 10000;
+
+export function getVariationForExperiment(experimentName) {
+
+}
