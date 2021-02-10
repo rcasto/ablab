ae288e42c33024ab67122b745956d1e90f1aa167
Richie Casto <rcasto92@gmail.com>
2021-02-09 20:43:13 -0500

    add typescript/rollup support

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import typescript from '@rollup/plugin-typescript';
+
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: {
         file: 'dist/index.js',
         format: 'es'
-    }
+    },
+    plugins: [
+        typescript(),
+    ],
 };
