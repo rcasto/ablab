e5bfdaeff5e5c31220329413f6a5e03557c384c9
Richie Casto <rcasto92@gmail.com>
2021-03-02 21:10:10 -0500

    restructure/organize cli tests

--- src/cli.test.ts
+++ src/cli.test.ts
@@ -25,81 +25,85 @@ async function runCliWithArgs(...args: string[]) {
 
 describe('lab cli tests', () => {
 
-    it('can handle no args provided', async () => {
-        try {
-            await runCliWithArgs();
-
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        } catch (err) {
-            expect(err.code).toEqual(1);
-            expect(err.stdout).toEqual('');
-            expect(err.stderr).not.toEqual('');
-        }
+    describe('cli error scenarios', () => {
+        it('can handle no args provided', async () => {
+            try {
+                await runCliWithArgs();
+    
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            } catch (err) {
+                expect(err.code).toEqual(1);
+                expect(err.stdout).toEqual('');
+                expect(err.stderr).not.toEqual('');
+            }
+        });
+    
+        it('can handle case where only 1 arg (instead of 2) is provided', async () => {
+            try {
+                await runCliWithArgs('fake-command');
+    
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            } catch (err) {
+                expect(err.code).toEqual(1);
+                expect(err.stdout).toEqual('');
+                expect(err.stderr).not.toEqual('');
+            }
+        });
+    
+        it('can handle case where at given experiment config path does not exist', async () => {
+            try {
+                await runCliWithArgs('fake-command', 'fake-experiment-config-path');
+    
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            } catch (err) {
+                expect(err.code).toEqual(1);
+                expect(err.stdout).toEqual('');
+                expect(err.stderr).not.toEqual('');
+            }
+        });
+    
+        it('can handle case where given experiment config path does not point to valid JSON', async () => {
+            try {
+                await runCliWithArgs('fake-command', nonJSONExperimentConfigPath);
+    
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            } catch (err) {
+                expect(err.code).toEqual(1);
+                expect(err.stdout).toEqual('');
+                expect(err.stderr).not.toEqual('');
+            }
+        });
+    
+        it('can handle case where at given command is invalid', async () => {
+            try {
+                await runCliWithArgs('fake-command', validExperimentConfigPath);
+    
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            } catch (err) {
+                expect(err.code).toEqual(1);
+                expect(err.stdout).toEqual('');
+                expect(err.stderr).not.toEqual('');
+            }
+        });
     });
 
-    it('can handle case where only 1 arg (instead of 2) is provided', async () => {
-        try {
-            await runCliWithArgs('fake-command');
-
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        } catch (err) {
-            expect(err.code).toEqual(1);
-            expect(err.stdout).toEqual('');
-            expect(err.stderr).not.toEqual('');
-        }
-    });
-
-    it('can handle case where at given experiment config path does not exist', async () => {
-        try {
-            await runCliWithArgs('fake-command', 'fake-experiment-config-path');
-
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        } catch (err) {
-            expect(err.code).toEqual(1);
-            expect(err.stdout).toEqual('');
-            expect(err.stderr).not.toEqual('');
-        }
-    });
-
-    it('can handle case where given experiment config path does not point to valid JSON', async () => {
-        try {
-            await runCliWithArgs('fake-command', nonJSONExperimentConfigPath);
-
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        } catch (err) {
-            expect(err.code).toEqual(1);
-            expect(err.stdout).toEqual('');
-            expect(err.stderr).not.toEqual('');
-        }
-    });
-
-    it('can handle case where at given command is invalid', async () => {
-        try {
-            await runCliWithArgs('fake-command', validExperimentConfigPath);
-
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        } catch (err) {
-            expect(err.code).toEqual(1);
-            expect(err.stdout).toEqual('');
-            expect(err.stderr).not.toEqual('');
-        }
-    });
-
-    it('can validate experiment config - no errors', async () => {
-        try {
-            const {
-                stdout
-            } = await runCliWithArgs('validate', validExperimentConfigPath);
-
-            expect(stdout).toEqual('{}\n');
-        } catch (err) {
-            // This shouldn't be reached
-            expect(true).toBeFalsy();
-        }
+    describe('validate cli command tests', () => {
+        it('can validate experiment config - no errors', async () => {
+            try {
+                const {
+                    stdout
+                } = await runCliWithArgs('validate', validExperimentConfigPath);
+    
+                expect(stdout).toEqual('{}\n');
+            } catch (err) {
+                // This shouldn't be reached
+                expect(true).toBeFalsy();
+            }
+        });
     });
 });
