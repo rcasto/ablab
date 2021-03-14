7ccdc3d2e9ae815657ed7b574b52fefeb89326ff
Richie Casto <rcasto92@gmail.com>
2021-03-13 20:54:07 -0500

    remove .js sample config, use JSON, read JSON in example

--- sample-integration/example.js
+++ sample-integration/example.js
@@ -1,18 +1,25 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createExperimenter } from 'lab';
+// import lab from './node_modules/lab/dist/lab.es.js';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
-import { sampleExperimentConfig } from './sample_experiment_config.js';
 import { stopwatch } from './performanceUtil.js';
 
 const numSimulations = 100;
 const numCyclesPerSimulation = 100000;
 let averageTimeToCompleteInMs = 0;
 
+const experimentConfigPath = resolve('./sample-experiment-config.json');
+const experimentConfig = JSON.parse(readFileSync(experimentConfigPath, {
+    encoding: 'utf-8'
+}));
+
 for (let i = 0; i < numSimulations; i++) {
     const { totalTimeInMs, result: variationCounts } = stopwatch(() => {
 
         const variationCounts = {};
-        const experimenter = createExperimenter(sampleExperimentConfig);
+        const experimenter = createExperimenter(experimentConfig);
 
         for (let i = 0; i < numCyclesPerSimulation; i++) {
             const uniqueId = uuidv4();
